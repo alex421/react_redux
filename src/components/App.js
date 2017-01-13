7a5b56b1b52854f2a821987b2263d7cddddae253
alex421 <alex421@gmail.com>
2017-01-13 16:58:37 +0200

    initial app structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,12 @@
 //this Component is used on every page and loads the other components
 
 import  React, {PropTypes} from 'react';
-
+import Header from './common/Header';
 class App extends React.Component {
   render() {
     return (
       <div className="container-fluid">
-        <p> Header here...</p>
+        <Header />
         {this.props.children}
       </div>
     );
